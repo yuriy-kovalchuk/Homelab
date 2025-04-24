80b796a6072a62178f2e41b08a5050b65933522c
yuriy <yuriykovalchuk1992@icloud.com>
2025-04-25 00:02:14 +0200

    minor fixes

--- k3s/install-k3s-v3.sh
+++ k3s/install-k3s-v3.sh
@@ -8,7 +8,7 @@ MASTER_USER="master"
 MAIN_MASTER_NODE="10.0.8.20"
 MASTER_NODES=()  # List of master node IP addresses
 WORKER_NODES=("10.0.8.24")  # List of worker node IP addresses
-K3S_CLUSTER_TOKEN="YOUR_CLUSTER_TOKEN"  # Replace with your desired K3s cluster token
+K3S_CLUSTER_TOKEN=${K3S_VAR_CLUSTER_TOKEN}  # Replace with your desired K3s cluster token
 
 # kubeconfig
 REMOTE_KUBECONFIG_PATH="/etc/rancher/k3s/k3s.yaml"  # Path to the Kubeconfig on the remote node
