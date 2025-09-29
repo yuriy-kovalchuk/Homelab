072aed22382a6eb2489e5f2f7e2b7aeeb2b547cf
yuriy <yuriykovalchuk1992@icloud.com>
2025-09-29 23:05:27 +0200

    argo/app[authentik] test homepage auth

--- kubernetes/test/minio.sh
+++
@@ -1,5 +0,0 @@
-kubectl create ns boutique
-kubectl apply -n boutique \
-  -f https://raw.githubusercontent.com/GoogleCloudPlatform/microservices-demo/main/release/kubernetes-manifests.yaml
-
-
--- kubernetes/test/tempo.sh
+++
@@ -1,4 +0,0 @@
-helm install tempo grafana/tempo \
-  --namespace tempo \
-  --set tempo.distributor.receivers.otlp.protocols.grpc=true \
-  --set tempo.distributor.receivers.otlp.protocols.http=true
