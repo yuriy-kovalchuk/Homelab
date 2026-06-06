20fd81a12b423c896461ddd57b6f5a33230f6dd3
yuriy <yuriykovalchuk1992@icloud.com>
2026-06-06 13:38:08 +0200

    feat: first deploy grafana tempo

--- kubernetes/clusters/workload-prd/terraform/vault/variables.tf
+++ kubernetes/clusters/workload-prd/terraform/vault/variables.tf
@@ -12,3 +12,13 @@ variable "forgejo_admin_email" {
   type      = string
   sensitive = true
 }
+
+variable "tempo_s3_access_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "tempo_s3_secret_key" {
+  type      = string
+  sensitive = true
+}
